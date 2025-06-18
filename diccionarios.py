1eed19eacc6d5d579da3ba8c43f1d5e30c880c4e
dakcl222 <crusofics@gmail.com>
2025-06-18 17:09:26 -0400

    prueba

--- diccionarios.py
+++ diccionarios.py
@@ -1,34 +1,54 @@
-dic={"nombre": "Diego",
-     "numero": 79873432,
-     "casado": True
-     }
-print(dic)
-dic["ciudad"]="santiago"
-print(dic)
+# dic={"nombre": "Diego",
+#      "numero": 79873432,
+#      "casado": True
+#      }
+# print(dic)
+# dic["ciudad"]="santiago"
+# print(dic)
 
-for key, value in dic.items():
-    print(key, value)
+# for key, value in dic.items():
+#     print(key, value)
 
-#print(value)
+# #print(value)
 
-print(dic["numero"])
+# print(dic["numero"])
 
-personas=[{"nombre": "Diego",
-     "numero": 7777777,
-     "casado": False
-     },
-     {"nombre": "hola",
-     "numero": 888888,
-     "casado": True
-     },
-     {"nombre": "adios",
-     "numero": 99999,
-     "casado": False
-     }]
+# personas=[{"nombre": "Diego",
+#      "numero": 7777777,
+#      "casado": False
+#      },
+#      {"nombre": "hola",
+#      "numero": 888888,
+#      "casado": True
+#      },
+#      {"nombre": "adios",
+#      "numero": 99999,
+#      "casado": False
+#      }]
 
-print(personas[2]["numero"])
+# print(personas[2]["numero"])
 
 
-text=input("Ingrese un texto: ")
+# text=input("Ingrese un texto: ")
 
-pala=input(" ")
+# pala=input(" ")
+
+
+
+
+lista=[{"producto": "hola", "precio": 5000},
+       {"producto": "hola2", "precio": 6000},
+       {"producto": "hola3", "precio": 7000},
+       ]
+# print(lista[0]["producto"])
+
+# for i in lista:
+#     print(i)
+
+# for p,c in enumerate(lista):
+#     print(p,c[0]["producto"],c[0]["precio"])
+
+contador=0
+for  i in lista:
+    print(contador+1,".-",lista[contador]["producto"],lista[contador]["precio"])
+    contador=contador+1
--- funciones.py
+++ funciones.py
@@ -141,8 +141,8 @@
 # else:
 #     print("El alumno reprobo")
 
-# ##Crear un programa para calcular un descuento
-# ## pedir al usuario el precio, y el descuento a aplicar, mostrar los resultados
+##Crear un programa para calcular un descuento
+## pedir al usuario el precio, y el descuento a aplicar, mostrar los resultados
 # total=0
 
 # def descuento(n1,n2):
@@ -193,34 +193,110 @@
 #         case 3:
 #         case 4:
 
-lista=[]
+# total=0
+# lista=[]
+# def mostrar_lista(lista):
+#             contador=0
+#             for i in lista:
+#                 print(contador+1,".- ",lista[contador]["nombre"], "$",lista[contador]["precio"])
+#                 contador=contador+1
+
+# while True:
+#     print('''
+#     1.- Agregar producto
+#     2.- Mostrar productos
+#     3.- Actualizar producto
+#     4.- Borrar producto
+#     5.- Comprar producto
+#     6.- Salir
+#         ''')
+#     op=int(input("Seleccione una opción: "))
+#     match op:
+#         case 1:
+#             producto=str(input("Ingrese el nombre del producto: "))
+#             precio=int(input("Ingrese el valor del producto: "))
+#             lista.append({"nombre": producto, "precio":precio},)
+#         case 2:
+#             mostrar_lista(lista)
+#         case 3:
+#             mostrar_lista(lista)
+#             op=int(input("Seleccione el producto a modificar: "))
+#             producto=str(input("Ingrese el nombre del producto: "))
+#             precio=int(input("Ingrese el valor del producto: "))
+#             lista[op-1]["nombre"]=producto
+#             lista[op-1]["precio"]=precio
+#         case 4:
+#             mostrar_lista(lista)
+#             op=int(input("Ingrese el producto a borrar"))
+#             lista.pop(op-1)
+#         case 5:
+#               mostrar_lista(lista)
+#               op=int(input("Ingrese el producto a comprar: "))
+#               total=total+lista[op-1]["precio"]
+#               print("total: ", total)
+#         case 6:
+#               break
+            
+#         case _:
+#             print("invalido")
+
+#Crear programa CRUD del siguiente diccionario
+#  
+
+personas={
+    1:{"nombre": "Diego Rivera",
+       "numero":[11111,11112],
+       "estadocivil": "casado",
+       "trabajando": True,
+       "edad": 64},
+    2:{"nombre": "Hola hola",
+       "numero":[22222,22223],
+       "estadocivil": "soltero",
+       "trabajando": True,
+       "edad": 64},
+    3:{"nombre": "alo alo",
+       "numero":[33333,33334],
+       "estadocivil": "casado",
+       "trabajando": False,
+       "edad": 64},
+}
+
 
 while True:
     print('''
-    1.- Agregar producto
-    2.- Mostrar productos
-    3.- Actualizar producto
-    4.- Salir
-        ''')
-    op=int(input("Seleccione una opción: "))
+    1.- Ingresar personas
+    2.- Mostrar personas
+    3.- Actualizar persona
+    4.- Borrar persona
+    5.- Salir
+''')
+    op=int(input("Ingrese opcion: "))
     match op:
         case 1:
-            producto=str(input("Ingrese el nombre del producto: "))
-            precio=int(input("Ingrese el valor del producto: "))
-            lista.append({"nombre": producto, "precio":precio},)
+            nombre=str(input("Ingrese el nombre: "))
+            numero=int(input("Ingrese el numero: "))
+            estadocivil=str(input("Ingrese el estado civil: "))
+            trabajando=bool(input("Ingrese si esta trabajando o no: "))
+            edad=int(input("Ingrese la edad: "))
+            nextkey=len(personas)
+            personas[nextkey+1]={"nombre": nombre,
+             "numero":[numero],
+             "estadocivil": estadocivil,
+             "trabajando": trabajando,
+             "edad": edad},
+
         case 2:
-            contador=0
-            for i in lista:
-                print(contador+1,".- ",lista[contador]["nombre"], lista[contador]["precio"],"$")
+            contador=1
+            for i in personas:
+                print(contador,".-", personas[contador])
                 contador=contador+1
+
         case 3:
-            op=int(input("Seleccione el producto a modificar: "))
-            producto=str(input("Ingrese el nombre del producto: "))
-            precio=int(input("Ingrese el valor del producto: "))
-            lista[op-1]["nombre"]=producto
-            lista[op-1]["precio"]=precio
-        case 4:
-            break
+            op=int(input("Selecciona la persona a actualizar: "))
             
-        case _:
-            print("invalido")
+
+        case 4:
+            op=int(input("Selecciona la persona a borrar: "))
+
+        case 5:
+            break
